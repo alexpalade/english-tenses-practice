2d5a965ed2d0cfe062f76e65549453dab4069940
Alexandru Palade <alexpalade@gmail.com>
2019-03-01 23:15:06 +0200

    removed shuffle

--- script.js
+++ script.js
@@ -6,16 +6,6 @@ let verbs = [
     ['swim', 'swam'],
 ];
 
-function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
-}
-
-shuffle(verbs);
-
 let currentVerb = 0;
 let status = 'asking';
 
